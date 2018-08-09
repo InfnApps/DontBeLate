7712848315b63de37174d547786626755626251c
hallison.paz <hallison.paz@prof.infnet.edu.br>
2018-08-09 09:09:50 -0300

    base login and signup activities implemented

---
+++ app/src/main/java/br/edu/infnet/dontbelate/LoginActivity.java
@@ -0,0 +1,45 @@
+package br.edu.infnet.dontbelate;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    //
+    public void signIn(View view){
+        //TODO: sign in code
+
+        EditText field = findViewById(R.id.email_field);
+        String email = field.getText().toString();
+
+        field = findViewById(R.id.password_field);
+        String password = field.getText().toString();
+
+
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+
+        String savedPassword = preferences.getString(email, null);
+        if (savedPassword != null){
+            if (password.equals(savedPassword)){
+
+            }
+        }
+        //TODO: tratar o caso que não da certo
+    }
+
+
+    public void goToSignUp(View view){
+        startActivity(new Intent(this, SignUpActivity.class));
+    }
+}
---
+++ app/src/main/java/br/edu/infnet/dontbelate/SignUpActivity.java
@@ -0,0 +1,56 @@
+package br.edu.infnet.dontbelate;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+        Button button = findViewById(R.id.signup_button);
+
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //Sign Up button code
+
+                        EditText field = findViewById(R.id.email_field);
+                        String email = field.getText().toString();
+
+                        field = findViewById(R.id.email_confirmation_field);
+                        String confirmation = field.getText().toString();
+
+                        field = findViewById(R.id.password_field);
+                        String password = field.getText().toString();
+
+
+                        //TODO: Validate fields
+                        if (validateField()){
+                            // Se validou, posso armazenar um novo usuário
+                            SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+                            SharedPreferences.Editor editor  = preferences.edit();
+                            editor.putString(email,password);
+                            editor.commit();
+                        }
+                    }
+                }
+        );
+
+    }
+
+
+    private boolean validateField(){
+        //TODO: make it right!
+        return true;
+    }
+
+}
